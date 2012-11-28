3ee9af8bf4478c53893d73aedaae57f3f913a394
tech <tech@c7a306ab-1da8-41ca-89b9-360630dde085>
2012-11-28 11:13:36 +0000

    Added new parameters to cfg file. Corrected wrong comments.

--- branches/HTM-5.0-Sony/source/Lib/TLibCommon/TypeDef.h
+++ branches/HTM-5.0-Sony/source/Lib/TLibCommon/TypeDef.h
@@ -40,10 +40,10 @@
 
 //! \ingroup TLibCommon
 //! \{
-#define QC_MVHEVC_B0046                   0
-#define QC_IV_AS_LT_B0046                 1   //JCT3V-B0046: Disable 3DHEVC tools 
-#define QC_TMVP_IDX_MOD_B0046             1   //JCT3V-B0046: inter-view reference pictures are treated as long-term pictures, scaling of motion vectors for DCP based on ViewId/ViewOrderIdx is disabled 
-#define QC_REM_IDV_B0046                  1   //JCT3V-B0046: the reference index for temporal merging candidate is set to 0, as defined in HEVC
+#define QC_MVHEVC_B0046                   0   //JCT3V-B0046: disable 3DHEVC tools
+#define QC_IV_AS_LT_B0046                 1   //JCT3V-B0046: inter-view reference pictures are treated as long-term pictures, scaling of motion vectors for DCP based on ViewId/ViewOrderIdx is disabled  
+#define QC_TMVP_IDX_MOD_B0046             1   //JCT3V-B0046: the reference index for temporal merging candidate is set to 0, as defined in HEVC
+#define QC_REM_IDV_B0046                  1   //JCT3V-B0046: removal of IDV NAL unit type
 #define FIX_DEL_NULLPTR                   1
 
 #define DV_V_RESTRICTION_B0037            1   // JCT3V-B0037 disparity vector vertical range restriction
