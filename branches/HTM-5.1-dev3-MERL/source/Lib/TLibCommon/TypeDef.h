fd216900ebdff43c28e1f06806eca6b88a84d928
mitsubishi-htm <mitsubishi-htm@c7a306ab-1da8-41ca-89b9-360630dde085>
2013-01-26 18:41:17 +0000

    -Integration of JCT3V-C0152 & JCT3V-C0131 -This check-in is a full implemnentation of C0152, inlcuding C0131

--- branches/HTM-5.1-dev3-MERL/source/Lib/TLibCommon/TypeDef.h
+++ branches/HTM-5.1-dev3-MERL/source/Lib/TLibCommon/TypeDef.h
@@ -73,7 +73,7 @@
  */
 
 #define MERL_VSP_COMPENSATION_C0152          1 // JCT3V-C0152: 1: add VSP merge candidate to merging candidate list; 0: not to add   (nocand).
-#define MERL_MTK_VSP_DVP_REFINE_C0152_C0131  0 // JCT3V-C0152 && JCT3V-C0131: 1: refine disparity vector using a warped depth block; 0: not to refine  (mvp2off).
+#define MERL_MTK_VSP_DVP_REFINE_C0152_C0131  1 // JCT3V-C0152 && JCT3V-C0131: 1: refine disparity vector using a warped depth block; 0: not to refine  (mvp2off).
 
 #define MERL_VSP_BLOCKSIZE_C0152             4 // JCT3V-C0152: VSP block size, supported values: 1, 2 and 4.
 #define VSP_MERGE_POS                        5 // JCT3V-C0152: fixed position of VSP candidate in merge list, supported values: 5.
