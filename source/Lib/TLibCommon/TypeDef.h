5327f180d2b51e87fea504face063b3567f99a39
mediatek-htm <mediatek-htm@c7a306ab-1da8-41ca-89b9-360630dde085>
2013-01-31 06:39:49 +0000

    Integration of JCT3V-C0135 The MACRO is MTK_SIMPLIFY_DVTC

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -143,6 +143,7 @@
 #define DV_DERIVATION_PARALLEL_B0096      1   // JCT3V-B0096, enable parallel derivation of disparity vector
 #define QC_SIMPLE_NBDV_B0047              1   // JCT3V-B0047
 #define MTK_SAIT_TEMPORAL_FIRST_ORDER     1   // JCT3V-C00141/C0097
+#define MTK_SIMPLIFY_DVTC                 1   // JCT3V-C00135
 ///// ***** MOTION PARAMETER INHERITANCE  *********
 #define HHI_MPI                           1   // motion parameter inheritance from texture picture for depth map coding
 #if HHI_MPI
