c5f422e47b19a62f5a874c075c8465d700b1026c
lg <lg@c7a306ab-1da8-41ca-89b9-360630dde085>
2013-11-12 04:22:25 +0000

    JCT3V-F0159

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -222,7 +222,7 @@
                                               // LG_ZEROINTRADEPTHRESI_A0087
 #define QC_DIM_DELTADC_UNIFY_F0132        1   // Unify delta DC coding in depth intra modes
 #define HHI_DIM_PREDSAMP_FIX_F0171        1
-#define LGE_PRED_RES_CODING_DLT_DOMAIN_F0159 0 //JCT3V-F0159
+#define LGE_PRED_RES_CODING_DLT_DOMAIN_F0159 1 //JCT3V-F0159
 #endif
 ///// ***** VIEW SYNTHESIS PREDICTION *********
 #if H_3D_VSP
