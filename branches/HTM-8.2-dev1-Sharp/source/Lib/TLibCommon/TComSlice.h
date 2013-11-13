9bdfbd94c393f12b401f919e0f91ca28e1329aff
sharpjp-htm <sharpjp-htm@c7a306ab-1da8-41ca-89b9-360630dde085>
2013-11-13 07:14:41 +0000

    Integration of F0105

--- branches/HTM-8.2-dev1-Sharp/source/Lib/TLibCommon/TComSlice.h
+++ branches/HTM-8.2-dev1-Sharp/source/Lib/TLibCommon/TComSlice.h
@@ -1729,6 +1729,9 @@ private:
   Int        m_aiAlterRefIdx   [2]; 
 #endif
 #if H_3D_ARP
+#if SHARP_ARP_REF_CHECK_F0105
+  Bool m_arpRefPicAvailable[2][MAX_NUM_LAYERS];
+#endif
   TComList<TComPic*> * m_pBaseViewRefPicList[MAX_NUM_LAYERS];
   UInt                 m_nARPStepNum; 
 #if QC_MTK_INTERVIEW_ARP_F0123_F0108
@@ -1820,7 +1823,11 @@ public:
 #endif
 #if H_3D_ARP
   Void      setBaseViewRefPicList( TComList<TComPic*> *pListPic, Int iViewIdx )      { m_pBaseViewRefPicList[iViewIdx] = pListPic;                   }
+#if SHARP_ARP_REF_CHECK_F0105
+  Void      setARPStepNum( TComPicLists*ivPicLists );
+#else
   Void      setARPStepNum();                                 
+#endif
   TComPic*  getBaseViewRefPic    ( UInt uiPOC , Int iViewIdx )                       { return xGetRefPic( *m_pBaseViewRefPicList[iViewIdx], uiPOC ); }
   UInt      getARPStepNum( )                                                         { return m_nARPStepNum;                                         }  
 #endif
@@ -2024,6 +2031,9 @@ public:
 #if QC_MTK_INTERVIEW_ARP_F0123_F0108
   Int       getFirstTRefIdx        ( RefPicList e )                { return  m_aiFirstTRefIdx[e];     }
   Void      setFirstTRefIdx        ( RefPicList e, Int i )         { m_aiFirstTRefIdx[e]    = i;      }
+#endif
+#if SHARP_ARP_REF_CHECK_F0105
+  Bool      getArpRefPicAvailable( RefPicList e, Int viewIdx) {return m_arpRefPicAvailable[e][getVPS()->getLayerIdInNuh(viewIdx, 0)]; }
 #endif
   Void      setIsDepth            ( Bool isDepth )   { m_isDepth = isDepth; }
   Bool      getIsDepth            ()                 { return m_isDepth; }
