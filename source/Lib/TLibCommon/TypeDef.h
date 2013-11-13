be9f5e45ea154846f097f6b0b6928d18851a2483
lg <lg@c7a306ab-1da8-41ca-89b9-360630dde085>
2013-11-13 01:24:21 +0000

    Integration of "Removal of redundancy on VSP, ARP and IC" FJCT3V-F0104 MACRO "LGE_SHARP_VSP_INHERIT_F0104"
    By Taesup Kim (taesup.kim@lge.com)

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -232,6 +232,7 @@
 #define H_3D_VSP_CONSTRAINED              0
 #endif
 #define MTK_VSP_SIMPLIFICATION_F0111      1   // 1. Inherited VSP also use NBDV of current CU, 2. VSP cannot be inherited from above LCU rowss
+#define LGE_SHARP_VSP_INHERIT_F0104       1
 #endif
 
 ///// ***** ILLUMATION COMPENSATION *********
