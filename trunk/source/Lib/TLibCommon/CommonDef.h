e0e1e28e94661af79d6577830e287eb6fb9ee604
tech <tech@c7a306ab-1da8-41ca-89b9-360630dde085>
2013-12-13 16:30:12 +0000

    - Changed version number. - Renamed coding results sheets.

--- trunk/source/Lib/TLibCommon/CommonDef.h
+++ trunk/source/Lib/TLibCommon/CommonDef.h
@@ -59,7 +59,7 @@
 // ====================================================================================================================
 
 #if H_MV
-#define NV_VERSION        "9.1rc1"              ///< Current software version
+#define NV_VERSION        "9.1"              ///< Current software version
 #define HM_VERSION        "12.0"                ///< 
 #else
 #define NV_VERSION        "12.0"                 ///< Current software version
