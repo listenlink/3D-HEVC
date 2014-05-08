9b2b6dc2b4ba58da39a3cc2ba770709ee9cf839c
qualcomm <qualcomm@c7a306ab-1da8-41ca-89b9-360630dde085>
2014-05-08 22:28:59 +0000

    Implementation of JCT3V_H0056 -- Inference of sps_temporal_id_nesting_flag when it is not present.
    Macro: H0056_SPS_TEMP_NESTING_FIX
    
    Submitted by Hendry (fhendry@qti.qualcomm.com)

--- source/Lib/TLibCommon/TypeDef.h
+++ source/Lib/TLibCommon/TypeDef.h
@@ -361,7 +361,7 @@
 
 #define H0044_POC_LSB_NOT_PRESENT        1      ///< JCT3V-H0044: Add constraint checking on the value of poc_reset_idc and poc_lsb_val
 #define H0056_EOS_CHECKS                 1      ///< JCT3V-H0056: Put checks on handling EOS
-
+#define H0056_SPS_TEMP_NESTING_FIX       1      ///< JCT3V-H0056: Fix the inference value of sps_temporal_id_nesting_flag when it is not present
 /////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////   TBD                  //////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////
