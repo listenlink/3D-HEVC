495914b20b127cfa0f41271de2465ff24cf0776b
samsung-htm <samsung-htm@c7a306ab-1da8-41ca-89b9-360630dde085>
2014-07-15 01:25:46 +0000

    Integration of JCT3V-I0078: Disallowing DBBP in 8x8 CU

--- source/Lib/TLibDecoder/TDecEntropy.cpp
+++ source/Lib/TLibDecoder/TDecEntropy.cpp
@@ -139,9 +139,17 @@ Void TDecEntropy::decodePartSize( TComDataCU* pcCU, UInt uiAbsPartIdx, UInt uiDe
   
 #if H_3D_DBBP
 #if SEC_DBBP_EXPLICIT_SIG_I0077
+#if SEC_DBBP_DISALLOW_8x8_I0078
+  if( pcCU->getSlice()->getVPS()->getUseDBBP(pcCU->getSlice()->getLayerIdInVps()) && (pcCU->getPartitionSize(uiAbsPartIdx) == SIZE_2NxN || pcCU->getPartitionSize(uiAbsPartIdx) == SIZE_Nx2N) && pcCU->getWidth(uiAbsPartIdx) > 8 )
+#else
   if( pcCU->getSlice()->getVPS()->getUseDBBP(pcCU->getSlice()->getLayerIdInVps()) && (pcCU->getPartitionSize(uiAbsPartIdx) == SIZE_2NxN || pcCU->getPartitionSize(uiAbsPartIdx) == SIZE_Nx2N) )
+#endif
+#else
+#if SEC_DBBP_DISALLOW_8x8_I0078
+  if( pcCU->getSlice()->getVPS()->getUseDBBP(pcCU->getSlice()->getLayerIdInVps()) && pcCU->getWidth(uiAbsPartIdx) > 8 )
 #else
   if( pcCU->getSlice()->getVPS()->getUseDBBP(pcCU->getSlice()->getLayerIdInVps()) )
+#endif
 #endif
   {
     decodeDBBPFlag(pcCU, uiAbsPartIdx, uiDepth);
