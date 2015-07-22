34306bbf69b5a76833c8c98f2196404912c56f26
tech <tech@c7a306ab-1da8-41ca-89b9-360630dde085>
2015-07-22 12:12:41 +0000

    Enabled NH_3D_FULL_PEL_DEPTH_MAP_MV_ACC.

--- branches/HTM-14.1-update-dev2-Testing/source/Lib/TLibCommon/TypeDef.h
+++ branches/HTM-14.1-update-dev2-Testing/source/Lib/TLibCommon/TypeDef.h
@@ -86,7 +86,7 @@
                                               // LGE_CHROMA_IC_J0050_J0034
 
 #define NH_3D_IC_FIX                      0   // Fix uninitialized value in cDtParam.bUseIC (it happened on intra block)
-#define NH_3D_FULL_PEL_DEPTH_MAP_MV_ACC   0   // Full Pel Interpolation for Depth, HHI_FULL_PEL_DEPTH_MAP_MV_ACC
+#define NH_3D_FULL_PEL_DEPTH_MAP_MV_ACC   1   // Full Pel Interpolation for Depth, HHI_FULL_PEL_DEPTH_MAP_MV_ACC
 
 #define NH_3D_QTLPC                       0   // OL_QTLIMIT_PREDCODING_B0068 //JCT3V-B0068
                                               // HHI_QTLPC_RAU_OFF_C0160 JCT3V-C0160 change 2: quadtree limitation and predictive coding switched off in random access units 
